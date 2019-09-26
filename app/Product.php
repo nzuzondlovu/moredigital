63709ac566c02466f00ce8acf51e289ee2362573
Nzuzo Ndlovu <nzuzondlovu@gmail.com>
2019-09-26 20:20:45 +0200

    Added bid resources and updated models

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'sku', 'price', 'description', 'file'];
+
+    public function bids()
+    {
+        return $this->hasMany('App\Bid', 'product_id', 'id'));
+    }
 }
---
+++ database/migrations/2019_09_26_181042_create_bids_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('product_id');
+            $table->string('email');
+            $table->decimal('amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
